70a11c21c8d1ed2d9935a80a1f18d7c4fb49990a
Guillec Matteo <matteo.guillec@student-cs.fr>
2024-06-05 14:03:08 +0200

    blabla

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,48 +1,24 @@
 import logo from './logo.svg';
 import './Home.css';
-import {useState} from 'react';
-import {useEffect} from 'react';
-import axios from 'axios';
+import { useState } from 'react';
+import {useFetchMovies} from './useFetchMovies.jsx'
+import MovieCards from '../../components/Movie.jsx';
 
 function Home() {
-  const [movieName, setMovieName] = useState('');
-
-  const handleInputChange = (event) => {
-    setMovieName(event.target.value);
-
-  const [movies,setMovies] = useState([])
-  };
-
-  useEffect(() => {
-    console.log("Le composant Home a été mis à jour");
-  }, []);
-
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjlmNjAwMzY4MzMzODNkNGIwYjNhNzJiODA3MzdjNCIsInN1YiI6IjY0NzA5YmE4YzVhZGE1MDBkZWU2ZTMxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Em7Y9fSW94J91rbuKFjDWxmpWaQzTitxRKNdQ5Lh2Eo'
-  }
-  };
-
-  axios
-  .get(`https://developer.themoviedb.org/reference/movie-top-rated-list`, options)
-  .then((response) => {
-		movies=response
-    console.log(response)
-  })
-  .catch((error) => {
-		// Do something if call failed
-		console.log(error)
-  });
-
-  return ( 
+  const [movie_name, set_movie_name] = useState('');
+  const {movie_list} = useFetchMovies();
+  const listMovies = movie_list.map(movie => <li key={movie.id}>{movie.title}</li>);
+  
+  return (
     <div className="App">
       <header className="App-header">
+        <h1>Meilleur film de tous les temps</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Top 100 best films
+          Edit <code>src/App.jsx</code> and save to reload.
         </p>
+        <input name="Nom de Film" value={movie_name} onChange={e => set_movie_name(e.target.value)}/>
+        <MovieCards movie_list={movie_list}/>
         <a
           className="App-link"
           href="https://react.dev"
@@ -51,14 +27,8 @@ const options = {
         >
           Learn React
         </a>
+        
       </header>
-      <input 
-        type="text"
-        value={movieName}
-        placeholder="Film name"
-        onChange={handleInputChange}
-      />
-      <p> Film : {movieName} </p>
     </div>
   );
 }
